72004ddc1166c2cfefc23fa22c985cc27b5f731c
Elias Carvalho <eliascarv1998@gmail.com>
2021-07-11 10:32:55 -0300

    Versão 2

--- readexcel.py
+++ readexcel.py
@@ -1,15 +1,12 @@
 from openpyxl import load_workbook
 import pandas as pd
 import requests
-import pprint
 
 excel_file = 'sudocontrol-original.xlsx' 
 wb = load_workbook(excel_file, data_only = True)
 sh = wb['Abraçadeira (2)']
 wb.sheetnames
 sh['A1'].fill.start_color.index
-
-
 
 color_list = []
 for i in range(7, sh.max_row):
@@ -18,22 +15,26 @@ for i in range(7, sh.max_row):
         color_list.append(color_index)
 
 
-print(color_list)
 numrows = len(color_list)
 wbpd = pd.read_excel(excel_file, skiprows=5, sheet_name='Abraçadeira (2)', converters={'Pregão':str,'Cód. Unidade':str})
 wbpd = wbpd[0:numrows]
-color_col = [1 if i == 9 else 0 for i in color_list]
+color_col = [1 if i == 'FFE2F0D9' else 0 for i in color_list]
 wbpd['Ativo'] = color_col
 
-wbpd["N_PREGAO"] = wbpd['Cód. Unidade'] + "000" + wbpd['Pregão']
+wbpd["Número do Pregão"] = wbpd['Cód. Unidade'] + "000" + wbpd['Pregão']
 
 quant_item = []
 
 for index, row in wbpd.iterrows():
     if row['Ativo'] == 1:
-        num_item = int(row['Item'])
+        if row['Modalidade Compra'] == 'Dispensa de Licitação':
+            quant_item.append("Erro: Dispensa de Licitação")
+
         try:
-            response = requests.get('http://compras.dados.gov.br/pregoes/doc/pregao/{}/itens.json'.format(row["N_PREGAO"]))
+            num_item = int(row['Item'])
+            quant = 0
+
+            response = requests.get('http://compras.dados.gov.br/pregoes/doc/pregao/{}/itens.json'.format(row["Número do Pregão"]))
             resdict = response.json()
 
             itens = resdict['_embedded']['pregoes']
@@ -42,42 +43,34 @@ for index, row in wbpd.iterrows():
                 item_title = item['_links']['self']['title']
                 if int(item_title.split()[1].split(":")[0]) == num_item:
                     quant = int(item['quantidade_item'])
+                    quant_item.append(quant)
 
-            quant_item.append(quant)
-            quant = 0      
+            if quant == 0:
+                response = requests.get('http://compras.dados.gov.br/pregoes/doc/pregao/{}/itens.json?offset=500'.format(row["Número do Pregão"]))
+                resdict = response.json()
+
+                itens = resdict['_embedded']['pregoes']
+
+                for item in itens:
+                    item_title = item['_links']['self']['title']
+                    if int(item_title.split()[1].split(":")[0]) == num_item:
+                        quant = int(item['quantidade_item'])
+                        quant_item.append(quant)
+        
         except:
-            response = requests.get('http://compras.dados.gov.br/pregoes/doc/pregao/{}/itens.json?offset=500'.format(row["N_PREGAO"]))
-            resdict = response.json()
-
-            itens = resdict['_embedded']['pregoes']
-
-            for item in itens:
-                item_title = item['_links']['self']['title']
-                if int(item_title.split()[1].split(":")[0]) == num_item:
-                    quant = int(item['quantidade_item'])
+            quant_item.append("Erro: Desconhecido")
             
-            quant_item.append(quant)
-
     else:
         quant_item.append('Item não ativo')
         
 
 len(quant_item)
 
-response = requests.get('http://compras.dados.gov.br/pregoes/doc/pregao/1350260001312020/itens.json')
-resdict = response.json()
+wbpd['Quant Item'] = quant_item
 
-itens = resdict['_embedded']['pregoes']
-
-for item in itens:
-    item_title = item['_links']['self']['title']
-    if int(item_title.split()[1].split(":")[0]) == 803:
-        quant = item['quantidade_item']
-
-quant
-
-pdf_links = []
+anexos_links = []
 for index, row in wbpd.iterrows():
-    pdf_links.append("https://comprasnet.gov.br/livre/pregao/anexosDosItens.asp?uasg={}&numprp={}&prgcod=863000".format(row['Cód. Unidade'], row['Pregão']))
+    anexos_links.append("https://comprasnet.gov.br/livre/pregao/anexosDosItens.asp?uasg={}&numprp={}&prgcod=863000".format(row['Cód. Unidade'], row['Pregão']))
 
-pdf_links
+wbpd['Anexos Pregão'] = anexos_links
+
